f2a61a3a5b3b3b70df05d41b14ffa2e976713d7b
Peter Möller <peter.moller@cs.lth.se>
2025-06-16 14:15:57 +0200

    Now changes _all_ occurences of '000m' 🫣

--- sleep_info.sh
+++ sleep_info.sh
@@ -372,7 +372,7 @@ printf "${ESC}${BoldFace};${UnderlineFace}mPower Settings:$Reset\n"
 echo "The computer is running on: $PowerKind"
 [[ -n "$BatteryDetailsText" ]]           && echo "• Battery at $BatteryDetailsText"
 [[ -n "$BatteryHealth" ]]                && echo "• Battery capacity: $BatteryHealth"
-[[ -n "$AdapterDetails" ]]               && echo "• Adapter details: $(echo "$AdapterDetails" | sed 's/000m//; s/500m/.5/')"
+[[ -n "$AdapterDetails" ]]               && echo "• Adapter details: $(echo "$AdapterDetails" | sed 's/000m//g; s/500m/.5/g')"
 # The three below will be either 'n' (minutes) or '0' (no idlesleep)
 SystemSleepTimeOut="$(echo "$PMSET_G" | grep -E "^\ *sleep\ *" | awk '{print $2}')"
 DisplaySleepTimeOut="$(echo "$PMSET_G" | grep -E "^\ *displaysleep\ *" | awk '{print $2}')"
